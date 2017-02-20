8b7474bb41f699908b7f161eac74556ccf978974
Taemin Kim <flatcoke89@gmail.com>
2017-02-20 23:58:04 +0900

    set config setting according to environment value

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
 
 app = Flask(__name__)
 
-app.config.from_object('config.ProductionConfig')
+app.config.from_object(os.environ.get("FLATCOKE"))
 db = SQLAlchemy(app)
 manager = Manager(app)
 
