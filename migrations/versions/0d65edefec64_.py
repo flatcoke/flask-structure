24da2e2bec39890dce0afb8516fa8ca7f06fe96c
FLATCOKE <flatcoke89@gmail.com>
2019-02-26 22:57:37 +0900

    add migrations

---
+++ migrations/versions/0d65edefec64_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 0d65edefec64
+Revises: 
+Create Date: 2019-02-26 20:47:40.531000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0d65edefec64'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
